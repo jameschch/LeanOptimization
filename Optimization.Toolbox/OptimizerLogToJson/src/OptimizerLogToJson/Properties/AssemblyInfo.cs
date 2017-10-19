a164a16976ce8dc77ab064caef5fd55c9c9938b4
jameschch <jameschch@outlook.com>
2017-10-19 11:17:17 +0100

    Adds log to json tool

---
+++ Optimization.Toolbox/OptimizerLogToJson/src/OptimizerLogToJson/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OptimizerLogToJson")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("974e8af4-148e-4318-8025-c0286d819b50")]
